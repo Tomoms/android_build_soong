5400287c3ea678c18e4a9f8cac610cb009d3ed08
Pengxuan Zheng <pzheng@codeaurora.org>
2017-07-28 17:00:29 -0700

    Add Android.bp property sdclang
    The sdclang property in Android.bp is the equivalent of LOCAL_SDCLANG in
    Android.mk. It can be used in two scenarios.
    1) You may set sdclang to false to force individual modules to use AOSP LLVM
    even if SDCLANG is set to true globally.
    2) You may set sdclang to true to force individual modules to use SDLLVM even if
    SDCLANG is set to false globally.
    
    Note: You do not have to set this property if you do not intend to use a target
    compiler different from the default one. The default target compiler will be
    used if the sdclang property is not set.

--- cc/builder.go
+++ cc/builder.go
@@ -226,6 +226,7 @@ type builderFlags struct {
 	rsFlags       string
 	toolchain     config.Toolchain
 	clang         bool
+	sdclang       bool
 	tidy          bool
 	coverage      bool
 	sAbiDump      bool
@@ -388,7 +389,7 @@ func TransformSourceToObj(ctx android.ModuleContext, subdir string, srcFiles and
 		ccDesc := ccCmd
 
 		if flags.clang {
-			if ctx.Device() && config.SDClang {
+			if flags.sdclang {
 				ccCmd = "${config.SDClangBin}/" + ccCmd
 				extraFlags = " ${config.SDClangFlags}"
 			} else {
@@ -577,7 +578,7 @@ func TransformObjToDynamicBinary(ctx android.ModuleContext,
 	var ldCmd string
 	var extraFlags string
 	if flags.clang {
-		if ctx.Device() && config.SDClang {
+		if flags.sdclang {
 			ldCmd = "${config.SDClangBin}/clang++"
 			extraFlags = " ${config.SDClangFlags}"
 		} else {
@@ -729,7 +730,7 @@ func TransformObjsToObj(ctx android.ModuleContext, objFiles android.Paths,
 	var ldCmd string
         var extraFlags string
 	if flags.clang {
-		if ctx.Device() && config.SDClang {
+		if flags.sdclang {
 			ldCmd = "${config.SDClangBin}/clang++"
 			extraFlags = " ${config.SDClangFlags}"
 		} else {
