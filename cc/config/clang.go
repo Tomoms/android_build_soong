2a13b7ef7380a8bf15bb25b20de69ed07e07a747
mydongistiny <jaysonedson@gmail.com>
2018-11-15 14:50:09 -0800

    soong: Update for latest clang-9 [1/2]
    Change-Id: I5da3af0642c2800a9f17be6f6ed9c57dc69d3216
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- cc/config/clang.go
+++ cc/config/clang.go
@@ -93,6 +93,7 @@ var ClangUnknownLldflags = sorted([]string{
 var ClangLibToolingUnknownCflags = []string{
 	"-flto*",
 	"-fsanitize*",
+	"-fno-sanitize=implicit-integer-sign-change",
 }
 
 func init() {
@@ -153,6 +154,7 @@ func init() {
 	}, " "))
 
 	pctx.StaticVariable("ClangExtraNoOverrideCflags", strings.Join([]string{
+
 		"-Werror=address-of-temporary",
 		// Bug: http://b/29823425 Disable -Wnull-dereference until the
 		// new cases detected by this warning in Clang r271374 are
