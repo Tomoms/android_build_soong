d70846328fdcf243fdb24c8ac0501cc164fa909c
Juhyung Park <qkrwngud825@gmail.com>
2022-06-12 02:45:42 +0900

    build: soong: use jemalloc by default and allow opt-in to scudo (2/4)
    Also, clean up broken DisableScudo/PRODUCT_DISABLE_SCUDO.
    
    After these changes, malloc_not_svelte is only used to override cflags
    in external/jemalloc_new. Change accordingly.
    
    Detailed reason for switching to jemalloc by default is written in the
    bionic commit.
    
    Change-Id: Id5afcf064a5db6011676dc7141c71fd5bb0007cb
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    
    [YAAP] update malloc_not_svelte struct
    
    Change-Id: If5cf72cc95325b3eaa7c1cd9d25c6e2e65a6db3c

--- android/variable.go
+++ android/variable.go
@@ -71,6 +71,15 @@ type variableProperties struct {
 			Enabled proptools.Configurable[bool] `android:"arch_variant,replace_instead_of_append"`
 		} `android:"arch_variant"`
 
+		Malloc_use_scudo struct {
+			Cflags              []string `android:"arch_variant"`
+			Shared_libs         []string `android:"arch_variant"`
+			Whole_static_libs   []string `android:"arch_variant"`
+			Exclude_static_libs []string `android:"arch_variant"`
+			Srcs                []string `android:"arch_variant"`
+			Header_libs         []string `android:"arch_variant"`
+		} `android:"arch_variant"`
+
 		Malloc_low_memory struct {
 			Cflags              []string `android:"arch_variant"`
 			Shared_libs         []string `android:"arch_variant"`
@@ -306,6 +315,7 @@ type ProductVariables struct {
 	Skip_boot_jars_check         *bool    `json:",omitempty"`
 	Malloc_low_memory            *bool    `json:",omitempty"`
 	Malloc_low_memory_libc32     *bool    `json:",omitempty"`
+	Malloc_use_scudo             *bool    `json:",omitempty"`
 	Malloc_zero_contents         *bool    `json:",omitempty"`
 	Malloc_pattern_fill_contents *bool    `json:",omitempty"`
 	Safestack                    *bool    `json:",omitempty"`
@@ -341,8 +351,6 @@ type ProductVariables struct {
 	EnableCFI       *bool    `json:",omitempty"`
 	CFIExcludePaths []string `json:",omitempty"`
 	CFIIncludePaths []string `json:",omitempty"`
-
-	DisableScudo *bool `json:",omitempty"`
 
 	MemtagHeapExcludePaths      []string `json:",omitempty"`
 	MemtagHeapAsyncIncludePaths []string `json:",omitempty"`
@@ -751,6 +759,7 @@ func (v *ProductVariables) SetDefaultConfig() {
 
 		Malloc_low_memory:            boolPtr(false),
 		Malloc_low_memory_libc32:     boolPtr(false),
+		Malloc_use_scudo:             boolPtr(false),
 		Malloc_zero_contents:         boolPtr(true),
 		Malloc_pattern_fill_contents: boolPtr(false),
 		Safestack:                    boolPtr(false),
