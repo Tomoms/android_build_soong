8e66c29303cbb582944ec3b372bd89f8143654d8
Jake Weinstein <jake@aospa.co>
2023-08-07 14:51:00 +0900

    [DO NOT SUBMIT] Add Kryo785 variant
    This is the same as Cortex-A510 but with SVE disabled
    because Qualcomm disables SVE in firmware on 8450/8550.
    
    Test: llvm-objdump -d
    Change-Id: I818a400a72c96b60d43a5e2ccf009fc948bd671c

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -90,6 +90,10 @@ var (
 			// Use cortex-a53 because kryo385 is not supported in clang.
 			"-mcpu=cortex-a53",
 		},
+		"kryo785": []string{
+			// Disable SVE instructions because Qualcomm disabled SVE in firmware.
+			"-mcpu=cortex-a510+nosve",
+		},
 		"exynos-m1": []string{
 			"-mcpu=exynos-m1",
 		},
@@ -127,6 +131,7 @@ func init() {
 	pctx.StaticVariable("Arm64CortexA53Cflags", strings.Join(arm64CpuVariantCflags["cortex-a53"], " "))
 	pctx.StaticVariable("Arm64CortexA55Cflags", strings.Join(arm64CpuVariantCflags["cortex-a55"], " "))
 	pctx.StaticVariable("Arm64KryoCflags", strings.Join(arm64CpuVariantCflags["kryo"], " "))
+	pctx.StaticVariable("Arm64Kryo785Cflags", strings.Join(arm64CpuVariantCflags["kryo785"], " "))
 	pctx.StaticVariable("Arm64ExynosM1Cflags", strings.Join(arm64CpuVariantCflags["exynos-m1"], " "))
 	pctx.StaticVariable("Arm64ExynosM2Cflags", strings.Join(arm64CpuVariantCflags["exynos-m2"], " "))
 	pctx.StaticVariable("Arm64CortexA510Cflags", strings.Join(arm64CpuVariantCflags["cortex-a510"], " "))
@@ -147,6 +152,7 @@ var (
 		"kryo385":    "${config.Arm64CortexA53Cflags}",
 		"exynos-m1":  "${config.Arm64ExynosM1Cflags}",
 		"exynos-m2":  "${config.Arm64ExynosM2Cflags}",
+		"kryo785":    "${config.Arm64Kryo785Cflags}",
 	}
 
 	arm64CpuVariantLdflags = map[string]string{
