c2323c26305f98a1a08e5daad6cc7d0976b3bb12
Tommaso Fonda <fondatommaso2@gmail.com>
2023-08-30 09:59:47 +0200

    Revert "Deprecating clang Property"
    This reverts commit c25213da6e34909226f910f4307291c82da5625b.
    
    Change-Id: I9b6a41258abb482af742fb893aac3a8e91dd0d2f

--- cc/cc.go
+++ cc/cc.go
@@ -1883,8 +1883,6 @@ func (c *Module) GenerateAndroidBuildActions(actx android.ModuleContext) {
 
 	if c.Properties.Clang != nil && *c.Properties.Clang == false {
 		ctx.PropertyErrorf("clang", "false (GCC) is no longer supported")
-	} else if c.Properties.Clang != nil && !ctx.DeviceConfig().BuildBrokenClangProperty() {
-		ctx.PropertyErrorf("clang", "property is deprecated, see Changes.md file")
 	}
 
 	flags := Flags{
