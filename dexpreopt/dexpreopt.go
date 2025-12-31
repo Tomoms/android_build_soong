4c353d06cbc5417bb423eec74b8b39db1d72afab
Tommaso Fonda <fondatommaso2@gmail.com>
2025-12-31 17:36:22 +0100

    Revert "use speed compiler filter for dexpreopt by default"
    This reverts commit 84260d80ebc2b847efabd3b1de6caef031181fa7.

--- dexpreopt/dexpreopt.go
+++ dexpreopt/dexpreopt.go
@@ -421,7 +421,7 @@ func dexpreoptCommand(ctx android.BuilderContext, globalSoong *GlobalSoongConfig
 		} else if global.DefaultCompilerFilter != "" {
 			compilerFilter = global.DefaultCompilerFilter
 		} else {
-			compilerFilter = "speed"
+			compilerFilter = "quicken"
 		}
 		if module.EnforceUsesLibraries {
 			// If the verify_uses_libraries check failed (in this case status file contains a
